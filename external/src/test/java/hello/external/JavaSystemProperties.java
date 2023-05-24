4b0312a17b1560cd0f36c9ed693098af3c74f0c1
myBabyGrand <amuro_ray@kakao.com>
2023-05-24 23:20:16 +0900

    ADD : external pjt. init

---
+++ external/src/test/java/hello/external/JavaSystemProperties.java
@@ -0,0 +1,25 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class JavaSystemProperties {
+
+    public static void main(String[] args) {
+        Properties properties = System.getProperties();
+        for (Object o : properties.keySet()) {
+            log.info("prop {}={}", o,System.getProperty(String.valueOf(o)));
+        }
+
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+
+        log.info("url = {}", url);
+        log.info("username = {}", username);
+        log.info("password = {}", password);
+
+    }
+}
