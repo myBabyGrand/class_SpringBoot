1a4760c4a331f6ffad008d942bddad698b508260
myBabyGrand <amuro_ray@kakao.com>
2023-07-03 22:50:27 +0900

    add module : traffic occurring(cpu, jvm memory, jdbc connection)

---
+++ actuator/src/main/java/hello/controller/TrafficController.java
@@ -0,0 +1,57 @@
+package hello.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class TrafficController {
+
+    @GetMapping("cpu")
+    public String cpu(){
+        log.info("cpu");
+        long value = 0;
+        for (int i = 0; i < 10000000000L; i++) {
+            value++;
+        }
+        return "value : " + value;
+    }
+
+    private List<String> list = new ArrayList<>();
+
+    @GetMapping("jvm")
+    public String jvm(){
+        log.info("jvm");
+        long value = 0;
+        for (int i = 0; i < 100000; i++) {
+            list.add(String.valueOf(value++));
+        }
+        return "value : " + value;
+    }
+
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("/jdbc")
+    public String jdbc() throws Exception{
+        log.info("jdbc");
+        Connection connection = dataSource.getConnection();
+        log.info("connection info : {}", connection);
+//        connection.close();
+        return "ok";
+    }
+
+    @GetMapping("/error-log")
+    public String errorLog(){
+        log.error("error occured!");
+        return "error";
+    }
+
+}
